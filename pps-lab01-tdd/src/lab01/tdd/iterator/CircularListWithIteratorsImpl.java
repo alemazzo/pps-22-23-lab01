1f3fc165607550c6967db998a53b989576253d86
Alessandro <alemazzoli97@gmail.com>
2023-02-27 11:45:40 +0100

    Created base implementation and interface

---
+++ pps-lab01-tdd/src/lab01/tdd/iterator/CircularListWithIteratorsImpl.java
@@ -0,0 +1,33 @@
+package lab01.tdd.iterator;
+
+import lab01.tdd.iterator.CircularListWithIterators;
+
+import java.util.Iterator;
+
+public class CircularListWithIteratorsImpl implements CircularListWithIterators {
+
+    @Override
+    public void add(int element) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Iterator<Integer> forwardIterator() {
+        return null;
+    }
+
+    @Override
+    public Iterator<Integer> backwardIterator() {
+        return null;
+    }
+}
---
+++ pps-lab01-tdd/test/CircularListWithIteratorsTest.java
@@ -0,0 +1,15 @@
+import lab01.tdd.iterator.CircularListWithIterators;
+import lab01.tdd.iterator.CircularListWithIteratorsImpl;
+import org.junit.jupiter.api.BeforeEach;
+
+public class CircularListWithIteratorsTest {
+
+    private CircularListWithIterators list;
+
+    @BeforeEach
+    void beforeEach() {
+        list = new CircularListWithIteratorsImpl();
+    }
+
+    
+}
