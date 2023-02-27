853f0f1c084335ea7ae1e31f7b72340716e829e3
Alessandro <alemazzoli97@gmail.com>
2023-02-27 10:41:13 +0100

    Refactor and extract FEES

--- pps-lab01-intellij-basic-example/src/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/SimpleBankAccountWithAtm.java
@@ -3,12 +3,15 @@ import lab01.example.model.BankAccount;
 import lab01.example.model.SimpleBankAccount;
 
 public class SimpleBankAccountWithAtm extends SimpleBankAccount {
+
+    final static int FEES = 1;
+
     public SimpleBankAccountWithAtm(final AccountHolder accountHolder, int amount) {
         super(accountHolder, amount);
     }
 
     @Override
     public void deposit(int userID, double amount) {
-        super.deposit(userID, amount - 1);
+        super.deposit(userID, amount - FEES);
     }
 }
