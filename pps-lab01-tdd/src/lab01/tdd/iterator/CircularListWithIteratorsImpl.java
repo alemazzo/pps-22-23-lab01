c34676c517a56ecc9429bf2a57f4889f64132e1e
Alessandro <alemazzoli97@gmail.com>
2023-02-27 11:55:26 +0100

    implemented forward iterator

--- pps-lab01-tdd/src/lab01/tdd/iterator/CircularListWithIteratorsImpl.java
+++ pps-lab01-tdd/src/lab01/tdd/iterator/CircularListWithIteratorsImpl.java
@@ -4,13 +4,37 @@ import lab01.tdd.CircularList;
 import lab01.tdd.CircularListImpl;
 import lab01.tdd.iterator.CircularListWithIterators;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.IntStream;
 
-public class CircularListWithIteratorsImpl extends CircularListImpl implements CircularListWithIterators {
+public class CircularListWithIteratorsImpl implements CircularListWithIterators {
+
+    private final List<Integer> elements = new ArrayList<>();
+
+    @Override
+    public void add(int element) {
+        this.elements.add(element);
+    }
+
+    @Override
+    public int size() {
+        return this.elements.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
 
     @Override
     public Iterator<Integer> forwardIterator() {
-        return null;
+        return this.isEmpty() ?
+                IntStream.of().iterator() :
+                IntStream.iterate(0, i -> i + 1)
+                        .map(i -> this.elements.get(i % this.elements.size()))
+                        .iterator();
     }
 
     @Override
--- pps-lab01-tdd/test/CircularListWithIteratorsTest.java
+++ pps-lab01-tdd/test/CircularListWithIteratorsTest.java
@@ -3,6 +3,9 @@ import lab01.tdd.iterator.CircularListWithIteratorsImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Iterator;
+
 public class CircularListWithIteratorsTest {
 
     private CircularListWithIterators list;
@@ -12,6 +15,24 @@ public class CircularListWithIteratorsTest {
         list = new CircularListWithIteratorsImpl();
     }
 
-    
+    private void addElements(int ...elems){
+        for (int elem : elems) {
+            this.list.add(elem);
+        }
+    }
+    @Test
+    void testForwardIterator() {
+        this.addElements(1, 2);
+        final Iterator<Integer> iterator = this.list.forwardIterator();
+        assertTrue(iterator.hasNext());
+        assertEquals(1, iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(2, iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(1, iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(2, iterator.next());
+    }
+
 
 }
