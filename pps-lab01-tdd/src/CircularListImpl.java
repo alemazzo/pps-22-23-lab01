cf83a7943e0c795bc2b5567727c1d6b05ce283fb
Alessandro <alemazzoli97@gmail.com>
2023-02-27 11:09:20 +0100

    Refactored next implementation

--- pps-lab01-tdd/src/CircularListImpl.java
+++ pps-lab01-tdd/src/CircularListImpl.java
@@ -1,24 +1,17 @@
 import lab01.tdd.CircularList;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class CircularListImpl implements CircularList {
 
     private final List<Integer> items = new ArrayList<>();
-    private Iterator<Optional<Integer>> itemsIterator;
+    private int nextElement = 0;
 
     @Override
     public void add(int element) {
         this.items.add(element);
-        this.itemsIterator = IntStream.iterate(0, x -> x + 1)
-                .boxed()
-                .map(el -> Optional.ofNullable(this.items.get(el % this.items.size())))
-                .iterator();
     }
 
     @Override
@@ -33,7 +26,9 @@ public class CircularListImpl implements CircularList {
 
     @Override
     public Optional<Integer> next() {
-        return this.itemsIterator.next();
+        return this.items.size() > this.nextElement ?
+                Optional.of(this.items.get(this.nextElement++)) :
+                Optional.empty();
     }
 
     @Override
