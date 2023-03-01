ff00874aade67667536a415170c4db3c0d433aef
Alessandro <alemazzoli97@gmail.com>
2023-03-01 09:32:19 +0100

    Refactored2

--- pps-lab01-tdd/test/CircularListWithIteratorsTest.java
+++ pps-lab01-tdd/test/CircularListWithIteratorsTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Iterator;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,28 +27,25 @@ public class CircularListWithIteratorsTest {
     void testForwardIterator() {
         this.addElements(1, 2);
         final Iterator<Integer> iterator = this.list.forwardIterator();
-        assertTrue(iterator.hasNext());
-        assertEquals(1, iterator.next());
-        assertTrue(iterator.hasNext());
-        assertEquals(2, iterator.next());
-        assertTrue(iterator.hasNext());
-        assertEquals(1, iterator.next());
-        assertTrue(iterator.hasNext());
-        assertEquals(2, iterator.next());
+        IntStream.range(0, 5).forEach(i -> {
+            assertTrue(iterator.hasNext());
+            assertEquals(1, iterator.next());
+            assertTrue(iterator.hasNext());
+            assertEquals(2, iterator.next());
+        });
     }
 
     @Test
     void testBackwardIterator() {
         this.addElements(1, 2);
         final Iterator<Integer> iterator = this.list.backwardIterator();
-        assertTrue(iterator.hasNext());
-        assertEquals(2, iterator.next());
-        assertTrue(iterator.hasNext());
-        assertEquals(1, iterator.next());
-        assertTrue(iterator.hasNext());
-        assertEquals(2, iterator.next());
-        assertTrue(iterator.hasNext());
-        assertEquals(1, iterator.next());
+        IntStream.range(0, 5).forEach(i -> {
+            assertTrue(iterator.hasNext());
+            assertEquals(2, iterator.next());
+            assertTrue(iterator.hasNext());
+            assertEquals(1, iterator.next());
+            assertTrue(iterator.hasNext());
+        });
     }
 
     @Test
