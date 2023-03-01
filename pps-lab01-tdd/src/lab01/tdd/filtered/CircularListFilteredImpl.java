637e0b52f101be5af57ac044e14bc1fb51d409eb
Alessandro <alemazzoli97@gmail.com>
2023-03-01 09:27:51 +0100

    Refactored

--- pps-lab01-tdd/src/lab01/tdd/filtered/CircularListFilteredImpl.java
+++ pps-lab01-tdd/src/lab01/tdd/filtered/CircularListFilteredImpl.java
@@ -3,15 +3,15 @@ package lab01.tdd.filtered;
 import lab01.tdd.CircularList;
 import lab01.tdd.CircularListImpl;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class CircularListFilteredImpl implements CircularListFiltered {
 
-    private final List<Integer> items = new ArrayList<>();
+    private final Set<Integer> items = new HashSet<>();
     private final CircularList list = new CircularListImpl();
 
     @Override
@@ -22,14 +22,13 @@ public class CircularListFilteredImpl implements CircularListFiltered {
 
     @Override
     public Optional<Integer> filteredNext(final Predicate<Integer> filter) {
-        if (this.items.stream().noneMatch(filter)) {
-            return Optional.empty();
-        }
-        return Stream.generate(this.list::next)
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .filter(filter)
-                .findFirst();
+        return this.items.stream().noneMatch(filter) ?
+                Optional.empty() :
+                Stream.generate(this.list::next)
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .filter(filter)
+                        .findFirst();
     }
 
 }
